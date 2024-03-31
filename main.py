103a1791f429eba7010d04463bdf9c13ccedeb6d
Anton Naumtsev <anton.naumtsev@jetbrains.com>
2024-03-31 22:00:34 +0200

    implement mod_inverse

--- main.py
+++ main.py
@@ -36,6 +36,22 @@ def random_coprime_number(n: int) -> int:  # gcd(result, n) = 1
     return x
 
 
+def extended_gcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = extended_gcd(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def mod_inverse(a, m):
+    g, x, y = extended_gcd(a, m)
+    if g != 1:
+        raise Exception('Modular inverse does not exist')
+    else:
+        return x % m
+
+
 def generate_keys() -> (PublicKey, PrivateKey):
     p = random_prime()
     q = random_prime()
@@ -46,14 +62,23 @@ def generate_keys() -> (PublicKey, PrivateKey):
     n = p * q
     m = (p - 1) * (q - 1)
     d = random_coprime_number(m)
-    e = sympy.mod_inverse(d, m)
-
+    e = mod_inverse(d, m)
     return PublicKey(e, n), PrivateKey(d, n)
 
 
-def encrypt(message: int, public_key: PublicKey):
-    return pow(message, public_key.e, public_key.n)
+def fast_pow(a: int, n: int, mod: int) -> int:
+    if n == 0:
+        return 1
+    elif n == 1:
+        return a % mod
+    else:
+        c = fast_pow(a, n // 2, mod)
+        return (c * c * (a if n & 1 else 1)) % mod
 
 
-def decrypt(ciphertext: int, private_key: PrivateKey):
-    return pow(ciphertext, private_key.d, private_key.n)
+def encrypt(message: int, public_key: PublicKey) -> int:
+    return fast_pow(message, public_key.e, public_key.n)
+
+
+def decrypt(ciphertext: int, private_key: PrivateKey) -> int:
+    return fast_pow(ciphertext, private_key.d, private_key.n)
